f251696146b66530875fcf0830fef4ba112cffad
jenny.jione.jang <jenny.jione.jang@gmail.com>
2024-05-02 15:20:47 +0900

    [N과 M (8)] 중복조합 - N개중에 M개 뽑기

--- 15657.py
+++ 15657.py
@@ -6,9 +6,12 @@
 2024.04.30 4:51pm~4:55pm (4분)
 
 문제 정리
-1. 중복조합 문제.
+1. N개 중에서 M개를 뽑는 중복조합 문제.
 2. (1 1)인 수열 허용하므로 -> 중복 가능 문제 => visited 필요 없음
 3. 비내림차순이므로 (1 7)은 ok, (7 1)은 no -> 조합 문제 => for문의 범위는 start 이상으로 계속 바뀜
+4. for문에서 i는 numbers의 인덱스를 의미한다.
+    (수정 전에는 numbers의 실제 수를 사용했는데 그게 코드를 더 복잡하게 만듦. 그래서 수정.)
+5. solve 함수의 매개변수는 numbers의 인덱스이다.
 """
 
 # 중복조합
@@ -24,9 +27,9 @@ def solve(start):
     if len(seq)==M:
         print(' '.join(map(str, seq)))
         return
-    for i in numbers[numbers.index(start):]:
-        seq.append(i)
+    for i in range(start, N):
+        seq.append(numbers[i])
         solve(i)
         seq.pop()
 
-solve(numbers[0])
+solve(0)
