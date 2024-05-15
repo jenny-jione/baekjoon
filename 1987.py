3e5c9231d5b7a155ee04f99d17cc88d4f99107ff
jenny.jione.jang <jenny.jione.jang@gmail.com>
2024-05-15 22:19:56 +0900

    @@다시풀기@@ [알파벳] 백트래킹, 시간초과, pypy

--- 1987.py
+++ 1987.py
@@ -1,13 +1,36 @@
-# 시간초과 해결하기
+"""
+문제 이름: 알파벳
+문제 링크: https://www.acmicpc.net/problem/1987
+
+타임라인
+2024.05.10 ~ 2024.05.15
+
+<처음 코드가 시간초과 난 이유>
+1. az_check 배열을 지금까지 지나온 A~Z를 저장하는 리스트로 씀. 그래서 매번 in 검사를 해야만 했음
+2. visited와 az_check를 각각 설정했는데 그럴 필요가 없었다.
+    방문한 좌표인지 검사하는 것 == az_check에 있는 알파벳인지 검사하는 것
+    즉 같은 과정을 중복해서 검사하게 됨
+3. python3 으로 제출하면 시간초과가 뜸.
+4. board에 입력 그대로 알파벳을 저장함
+
+<수정>
+1. visited와 az_check를 az라는 크기 26의 배열로 고침.
+    ord('A')-65==0이므로 az[0]은 A를 지나왔는지 저장하고, ... az[25]는 Z를 지나왔는지 저장함
+2. 1과 동일
+3. pypy로 제출하면 통과함..!ㅎ
+4. board에 알파벳 말고 0~25로 저장함
+"""
+
 
 R, C = map(int, input().split())
-visited = [[0] * C for _ in range(R)]
-visited[0][0] = 1
 board = []
+
 for _ in range(R):
     line = input().rstrip()
-    board.append([l for l in line])
-az_check = [board[0][0]]
+    board.append([ord(l)-65 for l in line])
+
+az = [0] * 26
+az[board[0][0]] = 1
 answer = 0
 
 dy = [-1, 1, 0, 0]
@@ -18,15 +41,12 @@ def move_check(ny, nx):
         return False
     if nx not in range(C):
         return False
-    if visited[ny][nx]:
-        return False
-    if board[ny][nx] in az_check:
+    if az[board[ny][nx]] == 1:
         return False
     return True
 
 def dfs(y, x):
     global answer
-    # print(f'visit {y},{x} -- {board[y][x]}')
     if answer == 26:
         return
     move = False
@@ -35,15 +55,12 @@ def dfs(y, x):
         nx = x + xi
         if not move_check(ny, nx):
             continue
-        visited[ny][nx] = 1
-        az_check.append(board[ny][nx])
+        az[board[ny][nx]] = 1
         dfs(ny, nx)
-        visited[ny][nx] = 0
-        az_check.pop()
+        az[board[ny][nx]] = 0
         move = True
     if not move:
-        # print(f'{y},{x} ({board[y][x]}) no move! -- from now:', len(az_check))
-        answer = max(answer, len(az_check))
+        answer = max(answer, sum(az))
         return
 
 dfs(0, 0)
