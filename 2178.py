b95a7c728a0a5c5953bc2756f552e7a5f175c637
jenny.jione.jang <jenny.jione.jang@gmail.com>
2024-06-13 12:15:44 +0900

    [미로 탐색] bfs, deque, visited[nxt] = visited[cur] + 1

--- 2178.py
+++ 2178.py
@@ -38,3 +38,42 @@ def bfs(i, j):
 
 bfs(0, 0)
 print(visited[-1][-1])
+
+
+
+# 다시 풀기
+"""
+타임라인
+2024.06.13 11:43am~11:56am (13분)
+2024.06.13 12:07pm~12:10pm (3분)  total: 16분
+
+<정리>
+"""
+
+from collections import deque
+N, M = map(int, input().split())
+board = []
+visited = [[0]*M for _ in range(N)]
+for _ in range(N):
+    board.append(list(map(int, input())))
+
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+def bfs():
+    q = deque([[0, 0]])
+    visited[0][0] = 1
+    while q:
+        y, x = q.popleft()
+        if y==N-1 and x==M-1:
+            print(visited[N-1][M-1])
+            break
+        for yi, xi in zip(dy, dx):
+            ny, nx = y+yi, x+xi
+            if ny not in range(N) or nx not in range(M):
+                continue
+            if visited[ny][nx]==0 and board[ny][nx]==1:
+                visited[ny][nx] = visited[y][x] + 1
+                q.append([ny, nx])
+                
+bfs()
